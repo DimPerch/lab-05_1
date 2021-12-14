665b679ca18032275280204553ec0bb860b7f1ed
Dimitry Perchatkin <perchatkindimitry@gmail.com>
2021-12-14 15:56:01 +0300

    v2

--- simanovich-lab05.c
+++ simanovich-lab05.c
@@ -35,56 +35,12 @@ int is_sep(char s[], int len_s, char c)
 
 int main()
 {
-    char s_main[MAX_LEN], s_sep[MAX_LEN];
-    int len_main, len_sep, maximum, last_word_index, i, f1, f2, result, count, max_count; 
+    char string[MAX_LEN], sep[MAX_LEN], matrix[MAX_LEN][MAX_LEN];
+    int string_len, sep_len;
 
-    printf("Input string of text:\n");
-    len_main = new_gets(s_main, MAX_LEN);
+    printf("Input string");
+    string_len = new_gets(string, MAX_LEN);
+    printf("Input seps");
+    sep_len = new_gets(sep, MAX_LEN);
 
-    printf("Input len of seps' string:\n");
-    len_sep = new_gets(s_sep, MAX_LEN);
-
-    maximum = 0;
-    result = 0;
-    last_word_index = 0;
-    count = 0;
-
-    // find the first word
-    while (f1 == 1)
-    {
-        last_word_index += 1;
-        f1 = is_sep(s_sep, len_sep, s_main[last_word_index]);
-    }
-
-    // main find
-    for (i = last_word_index; i <= len_main + 1; i++)
-    {
-        f1 = is_sep(s_sep, len_sep, s_main[i]);
-        f2 = is_sep(s_sep, len_sep, s_main[i + 1]);
-        // if the end of word is find
-        if (f1 == 0 && (f2 == 1 || i + 1 == len_main))
-        {
-            count += 1;
-            // if new word is longer
-            if (i - last_word_index >= maximum)
-            {
-                maximum = i - last_word_index;
-                result = last_word_index;
-                max_count = count;
-            }
-        }
-        // if the start of word is find
-        if (f1 == 1 && f2 == 0)
-        {
-            last_word_index = i + 1;
-        }
-    }
-
-    printf("The result of program:\nLonest word is: ");
-    for (i = result; i <= result + maximum; i++){
-        printf("%c", s_main[i]);
-    }
-    printf(". It have number %i\n", max_count);
-
-    return 0;
 }
