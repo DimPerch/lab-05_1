e05ef032e052708179f4901dc346385cae7dcf59
Dimitry Perchatkin <perchatkindimitry@gmail.com>
2021-12-14 16:24:01 +0300

    м3

--- simanovich-lab05.c
+++ simanovich-lab05.c
@@ -36,11 +36,35 @@ int is_sep(char s[], int len_s, char c)
 int main()
 {
     char string[MAX_LEN], sep[MAX_LEN], matrix[MAX_LEN][MAX_LEN];
-    int string_len, sep_len;
+    int string_len, sep_len, i, f1, f2, last_state, current_state;
 
-    printf("Input string");
+    printf("Input string:\n");
     string_len = new_gets(string, MAX_LEN);
-    printf("Input seps");
+    printf("Input seps:\n");
     sep_len = new_gets(sep, MAX_LEN);
+
+    for (i = 0; i < string_len; i++)
+    {
+        f1 = is_sep(sep, sep_len, string[i]);
+        f2 = is_sep(sep, sep_len, string[i + 1]);
+        printf("%c", string[i]);
+        last_state = 1;
+
+        if (f1 < f2)
+        // start word
+        {
+            current_state = 0;
+            printf("\n");
+        }
+
+        if (f1 > f2)
+        // finish word
+        {
+            current_state = 1;
+            printf("\n");
+        }
+
+        last_state = current_state;
+    }
 
 }
